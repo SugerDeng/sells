fd9cd99977d204a8d6d278a1602099e2b3f9ca76
SugerDeng <fangzhidaohao@gmail.com>
2018-01-11 23:18:46 +0800

    食物详情 更新

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -26,6 +26,26 @@
                 <h1 class="title">商品信息</h1>
                 <p class="text">{{food.info}}</p>
             </div>
+            <split></split>
+            <div class="rating">
+                <h1 class="title">商品评价</h1>
+                <ratingselect @ratingTypeSelect="ratingTypeSelect" @contentToggle="contentToggle" :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+                <div class="rating-wrapper">
+                    <ul v-show="food.ratings && food.ratings.length">
+                        <li v-show="needShow(rating.rateType, rating.text)" v-for="(rating, index) in food.ratings" :key="index" class="rating-item border-1px">
+                            <div class="user">
+                                <span class="name">{{rating.username}}</span>
+                                <img class="avatar" :src="rating.avatar" width="12px" height="12px">
+                            </div>
+                            <div class="time">{{rating.rateTime | formatDate}}</div>
+                            <p class="text">
+                                <span :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}
+                            </p>
+                        </li>
+                    </ul>
+                    <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+                </div>
+            </div>
         </div>
     </div>
   </transition>
@@ -37,7 +57,13 @@
   import BScroll from 'better-scroll';
   import cartcontrol from '../cartcontrol/cartcontrol.vue';
   import Vue from 'vue';
+  import {formatDate} from '../../common/js/date';
   import split from '../split/split.vue';
+  import ratingselect from '../ratingselect/ratingselect.vue';
+
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props: {
@@ -47,15 +73,27 @@
     },
     data() {
       return {
-          showFlag: false
+          showFlag: false,
+          selectType: ALL,
+          onlyContent: false,
+          desc: {
+              all: '全部',
+              positive: '推荐',
+              negative: '吐槽'
+          }
       }
     },
-    computed: {
-        
+    filters: {
+        formatDate(time) {
+            let date = new Date(time);
+            return formatDate(date, 'yyyy-MM-dd hh:mm');
+        }
     },
     methods: {
         show() {
             this.showFlag = true;
+            this.selectType = ALL;
+            this.onlyContent = false;
             this.$nextTick(() => {
                 if (!this.scroll) {
                     this.scroll = new BScroll(this.$refs.food, {
@@ -73,18 +111,37 @@
         },
         _drop() {
             this.$emit('cartAdd', event.target);
+        },
+        needShow(type, text) {
+            if (this.onlyContent && !text) {
+                return false;
+            }
+            if (this.selectType === ALL) {
+                return true;
+            } else {
+                return type == this.selectType;
+            }
+        },
+        ratingTypeSelect(type) {
+            this.selectType = type;
+        },
+        contentToggle(onlyContent) {
+            this.onlyContent = onlyContent;
         }
     },
     components: {
         price,
         foodextra,
         cartcontrol,
-        split
+        split,
+        ratingselect
     }
   };
 </script>
 
 <style lang="less">
+    @import '../../common/less/mixin.less';
+
     .food {
         position: fixed;
         left: 0;
@@ -173,6 +230,67 @@
                 padding: 0 8px;
                 color: rgb(77, 85, 93);
                 
+            }
+        }
+        .rating {
+            padding-top: 18px;
+            .title {
+                margin-left: 18px;
+                font-size: 14px;
+                color: rgb(7, 17, 27);
+                line-height: 14px;
+            }
+            .rating-wrapper {
+                padding: 0 18px;
+                .rating-item {
+                    position: relative;
+                    padding: 16px 0;
+                    .border-1px(rgba(7, 17, 27, 0.1));
+                    .user {
+                        position: absolute;
+                        right: 0;
+                        top: 16px;
+                        line-height: 12px;
+                        font-size: 0;
+                        .name {
+                            display: inline-block;
+                            margin-right: 6px;
+                            vertical-align: top;
+                            font-size: 10px;
+                            color: rgb(147, 153, 159);
+                        }
+                        .avatar {
+                            border-radius: 50%;
+                        }
+                    }
+                    .time {
+                        margin-bottom: 6px;
+                        line-height: 12px;
+                        font-size: 10px;
+                        color: rgb(147, 153, 159);
+                    }
+                    .text {
+                        font-size: 12px;
+                        color: rgb(7, 17, 27);
+                        line-height: 16px;
+                        .icon-thumb_up, .icon-thumb_down {
+                            margin-right: 4px;
+                            font-size: 12px;
+                            line-height: 16px;
+                        }
+                        .icon-thumb_up {
+                            color: rgb(0, 160, 220);
+                        }
+                        .icon-thumb_down {
+                            color: rgb(147, 153, 159);
+                        }
+                    }
+                }
+                .no-rating {
+                    padding: 16px 0;
+                    font-size: 12px;
+                    color: rgb(147, 153, 159);
+                }
             }
         }
     }
